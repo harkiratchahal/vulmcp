9d68673d9d79023fc42ce791307d66ab69171d1e
harkiratchahal <vhschahal@gmail.com>
2026-06-19 16:29:15 +0530

    Indent error fix

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from vulnixmcp.server import mcp
 
 def main():
     # Run with HTTP transport
-mcp.run(transport="http", host="127.0.0.1", port=9000)
+    mcp.run(transport="http", host="127.0.0.1", port=9000)
 
 if __name__ == "__main__":
     main()
