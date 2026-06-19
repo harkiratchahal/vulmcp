fe4d7dce518da362acba09102b4aa2ae9538b1f0
harkiratchahal <vhschahal@gmail.com>
2026-06-19 16:26:48 +0530

    Transport issue fix

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from vulnixmcp.server import mcp
 
 def main():
-    mcp.run()
+    # Run with HTTP transport
+mcp.run(transport="http", host="127.0.0.1", port=9000)
 
 if __name__ == "__main__":
     main()
